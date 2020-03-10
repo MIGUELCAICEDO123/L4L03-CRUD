e119336bfa665ea96c632ec32cd3e66003823d8b
MIGUEL CAICEDO <miguelcaicedo64@gmail.com>
2020-03-09 19:06:26 -0500

    base

--- persona.sql
+++ persona.sql
@@ -11,7 +11,24 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- La exportación de datos fue deseleccionada.
+
+-- Volcando estructura de base de datos para persona
+CREATE DATABASE IF NOT EXISTS `persona` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `persona`;
+
+-- Volcando estructura para tabla persona.persona
+CREATE TABLE IF NOT EXISTS `persona` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(80) DEFAULT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Volcando datos para la tabla persona.persona: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `persona` DISABLE KEYS */;
+INSERT INTO `persona` (`id`, `nombre`, `email`) VALUES
+	(1, 'Miguel Caicedo', 'miguelcaicedo64@gmail.com');
+/*!40000 ALTER TABLE `persona` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
